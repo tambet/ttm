9ab1496f908424d8180b5314e845e0854782c5dc
Tambet Masik <tambet.masik@gmail.com>
2009-09-29 00:34:37 +0300

    Users REST service

--- ttm.rb
+++ ttm.rb
@@ -16,25 +16,29 @@ end
 #  respond_with(@user = User.create(params[:user]))
 #end
 
+set :logging, true
 
-get '/users.*' do
+get '/users.xml' do
   User.all.to_xml
 end
 
-get '/users/:id.*' do
-  User.find(params[:id]).to_xml
-end
-
-post '/users.*' do
-  p params
+post '/users.xml' do
+  puts params
   User.create!(params[:user])
 end
 
-put '/users/:id.*' do
-  User.find(params[:id]).update_attributes(params[:user])
+get '/users/:id.xml' do
+  User.find(params[:id]).to_xml
 end
 
-delete '/users/:id.*' do
+put '/users/:id.xml' do
+  p params
+  user = User.find(params[:id])
+  user.update_attributes(params[:user])
+  user.to_xml
+end
+
+delete '/users/:id.xml' do
   User.find(params[:id]).destroy
 end
 
