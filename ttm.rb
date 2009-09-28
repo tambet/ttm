2156e556b7a38d68e856393871b6af3214464030
Tambet Masik <tambet.masik@gmail.com>
2009-09-28 07:23:57 +0300

    User REST services added

--- ttm.rb
+++ ttm.rb
@@ -8,6 +8,35 @@ get '/' do
   'Hello world!'
 end
 
-get '/users' do
+#get '/users' do
+#  respond_with(@users = User.all)
+#end
+
+#post '/users' do
+#  respond_with(@user = User.create(params[:user]))
+#end
+
+
+get '/users.*' do
   User.all.to_xml
 end
+
+get '/users/:id.*' do
+  User.find(params[:id]).to_xml
+end
+
+post '/users.*' do
+  p params
+  User.create!(params[:user])
+end
+
+put '/users/:id.*' do
+  User.find(params[:id]).update_attributes(params[:user])
+end
+
+delete '/users/:id.*' do
+  User.find(params[:id]).destroy
+end
+
+
+
