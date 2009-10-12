4e3d61f3672979c897f1a77c453789b2509c8ecf
Tambet Masik <tambet@apprise.eu>
2009-10-12 15:40:39 +0300

    Addresses API Added

--- app.rb
+++ app.rb
@@ -48,3 +48,24 @@ get '/customers/:customer_id/addresses.xml' do
   content_type 'application/xml', :charset => 'utf-8'
   Customer.find(params[:customer_id]).addresses.to_xml
 end
+
+post '/customers/:customer_id/addresses.xml' do
+  params.merge!(Crack::XML.parse(request.body.read))
+  p params
+  #Address.create(params[:address])
+end
+
+get '/customers/:customer_id/addresses/:id.xml' do
+  content_type 'application/xml', :charset => 'utf-8'
+  Address.find(params[:id]).to_xml
+end
+
+put '/customers/:customer_id/addresses/:id.xml' do
+  params.merge!(Crack::XML.parse(request.body.read))
+  address = Address.find(params[:id])
+  address.update_attributes(params[:address])
+end
+
+delete '/customers/:customer_id/addresses/:id.xml' do
+  Address.find(params[:id]).destroy
+end
